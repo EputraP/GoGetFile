12e1a46b1a0722562898d579787a92c123d4e051
EputraP <EputraP@gmail.com>
2023-06-26 06:57:33 +0700

    updated 20230626

--- main.go
+++ main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
+
+var tpl *template.Template
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
 
-func setupRoutes() {
-	// http.HandleFunc("/upload", uploadFile)
-	http.ListenAndServe(":8080", nil)
+func main() {
+	fs := http.FileServer(http.Dir("./image"))
+	http.Handle("/", cors(fs))
+	fmt.Printf("Server Running")
+	http.ListenAndServe(":4352", nil)
 }
 
-func main() {
-	fmt.Println("Go File Upload Tutorial")
-	setupRoutes()
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "hello.html", nil)
+}
+
+func cors(fs http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// do your cors stuff
+		// return if you do not want the FileServer handle a specific request
+		enableCors(&w)
+		fs.ServeHTTP(w, r)
+	}
 }
