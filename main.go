58949e4ad82830291012be3b54ed11bcf3972112
EputraP <EputraP@gmail.com>
2023-06-26 09:21:21 +0700

    updated 20230626_3

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -15,12 +20,55 @@ func enableCors(w *http.ResponseWriter) {
 func main() {
 	fs := http.FileServer(http.Dir("./image"))
 	http.Handle("/", cors(fs))
+	http.HandleFunc("/image", GetImageHandler)
+	// construct JSON data
+	data := make(map[string]interface{})
+	data["image"] = "base64data"
+	data["barcodeFormat"] = 234882047
+	data["maxNumPerPage"] = 1
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.Post("/image2", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Printf("Server Running")
 	http.ListenAndServe(":4352", nil)
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "hello.html", nil)
+func GetImageHandler(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	fmt.Printf("Image Handler Running")
+	content, err := ioutil.ReadFile("image/wqwqwq.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var base64Encoding string
+
+	// Determine the content type of the image file
+	mimeType := http.DetectContentType(content)
+
+	// Prepend the appropriate URI scheme header depending
+	// on the MIME type
+	switch mimeType {
+	case "image/jpeg":
+		base64Encoding += "data:image/jpeg;base64,"
+	case "image/png":
+		base64Encoding += "data:image/png;base64,"
+	}
+
+	// Append the base64 encoded output
+	base64Encoding += toBase64(content)
+	fmt.Println(base64Encoding)
+	// fmt.Printf("file content: ", content)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	// jsonData := []byte(`{"status":` + base64Encoding + `}`)
+	jsonData := []byte(base64Encoding)
+	w.Write(jsonData)
+
 }
 
 func cors(fs http.Handler) http.HandlerFunc {
@@ -30,4 +78,7 @@ func cors(fs http.Handler) http.HandlerFunc {
 		enableCors(&w)
 		fs.ServeHTTP(w, r)
 	}
+}
+func toBase64(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
 }
